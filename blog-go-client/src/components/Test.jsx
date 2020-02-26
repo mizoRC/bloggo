96b11231d08450e3d9065ed6b6339cd7440f67f0
miguelrcDEV <mirodriguez11@gmail.com>
2020-02-26 23:02:17 +0100

    Editor and preview working

--- blog-go-client/src/components/Test.jsx
+++ blog-go-client/src/components/Test.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import MDEditor, { commands } from '@uiw/react-md-editor';
 
 const Test = () => {
-    const [ post, setPost ] = React.useState(''); 
+    const [ post, setPost ] = React.useState('**Hola**'); 
+
+    const changePost = value => {
+        setPost(value);
+    }
 
     return (
         <div
@@ -25,20 +30,27 @@ const Test = () => {
                     justifyContent: 'center'
                 }}
             >
-                <ReactMarkdown source={post} />
+                <MDEditor
+                    value={post}
+                    onChange={changePost}
+                    preview={'edit'}
+                    commands={[
+                        commands.bold, commands.italic, commands.strikethrough, commands.title, commands.hr, commands.divider, commands.link, commands.quote, commands.code, commands.image, commands.unorderedListCommand, commands.orderedListCommand, commands.checkedListCommand, commands.divider,
+                        commands.fullscreen
+                    ]}
+                    visiableDragbar={false}
+                    style={{width: '100%', height: '100%'}}
+                />
             </div>
 
             <div
                 style={{
                     width: '50%',
                     height: '100%',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'center'
+                    padding: '10px'
                 }}
             >
-            
+                <ReactMarkdown source={post} />
             </div>
         </div>
     )
